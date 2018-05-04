d9a8329758def8eacbc1f6afdf0707947cd5823f
CyrineSA <38528481+CyrineSA@users.noreply.github.com>
2018-05-04 17:11:28 +0200

    Cours et explication avec le prof d'informatqiue

--- PROJET INFORMATIQUE - NEW.py
+++ PROJET INFORMATIQUE - NEW.py
@@ -1,13 +1,38 @@
 #QUESTION 1 - NIVEAU 1
 
-exemplechaine = "Salut les amis ! On se retrouve pour un nouveau tuto beauté proposé par Gemey Maybelline"
-def fonction(compter_occurences):
-    A = 1
-    for lettre in exemplechaine:
-        if lettre == "caractère":
-            A = A + 1
-    return A
- 
-print(A)
+##exemplechaine = "Salut les amis ! On se retrouve caractere un nouveau tuto beauté proposé par Gemey Maybelline"
+##def compter_occurences(x):              #(x) = rpz notre argument
+##    A = 
+##    return A            #la fonction return nous de voir la valeur que nous donne la fonction
+## 
+##print(A)                #mettre print pour vraiment voir la valeur de la fonction
+##
+##
+##
+##chaine_y = "1,2,3,
 
-    
+def carre(toto):
+    return toto**2
+
+#nombre_utilisateur = input("Entrez votre nombre\n")
+nombre_utilisateur = 9
+
+x = 3
+x = x**2
+print (x)
+
+
+n = carre(nombre_utilisateur)
+print(n)
+
+
+print (nombre_utilisateur)
+
+
+
+texte = "banane"
+print (texte[2])  #les crochets nous permettent d'afficher le 2e caractère. Attention, cela commence à 0 !
+longueur = len(texte)  #nous permet de compter le nombre de lettres dans ma chaine
+print (longueur)
+
+
