66dcd911f8e673c1cc4adf1db8b2edf48ec996c0
CyrineSA <38528481+CyrineSA@users.noreply.github.com>
2018-05-04 14:56:46 +0200

    J'ai annulé ma modification

--- chaines.py
+++ chaines.py
@@ -2,7 +2,13 @@
 # encoding : utf-8
 
 
-
+#################################################################################################
+#   _                _         _                  _                           _   __            #
+#  | |   ___ ___  __| |_  __ _(_)_ _  ___ ___  __| |___   __ __ _ _ _ __ _ __| |_ \_\ _ _ ___   #
+#  | |__/ -_|_-< / _| ' \/ _` | | ' \/ -_|_-< / _` / -_) / _/ _` | '_/ _` / _|  _/ -_) '_/ -_)  #
+#  |____\___/__/ \__|_||_\__,_|_|_||_\___/__/ \__,_\___| \__\__,_|_| \__,_\__|\__\___|_| \___|  #
+#                                                                                               #
+#################################################################################################
 
 # Le but final de cette activité est d'écrire petit programme de cryptographie qui permet de chiffrer et déchiffrer des messages en utilisant le chiffrement de César. On devrait au final arriver notamment à déchiffrer des messages comme ceux-ci :
 
