ae108773c1598039608aca5adb771ed2865f1384
CyrineSA <38528481+CyrineSA@users.noreply.github.com>
2018-05-04 16:13:03 +0200

    Suite essai

--- PROJET INFORMATIQUE - NEW.py
+++ PROJET INFORMATIQUE - NEW.py
@@ -1,6 +1,13 @@
 #QUESTION 1 - NIVEAU 1
 
-exemplechaine = 
+exemplechaine = "Salut les amis ! On se retrouve pour un nouveau tuto beauté proposé par Gemey Maybelline"
 def fonction(compter_occurences):
-    compteur = 1
+    A = 1
+    for lettre in exemplechaine:
+        if lettre == "caractère":
+            A = A + 1
+    return A
+ 
+print(A)
+
     
