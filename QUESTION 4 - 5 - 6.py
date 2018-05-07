74bf5cbafe383f5c75ae53f53200f109cf51a4ac
Lucile <lucile.jacobe@u-psud.fr>
2018-05-07 16:55:08 +0200

    explication apporté à la question 4
    top moumoute !!!

--- QUESTION 4 - 5 - 6.py
+++ QUESTION 4 - 5 - 6.py
@@ -1,13 +1,13 @@
-#QUESTION 4, 5 et 6 - NIVEAU 1
+#QUESTION 4, 5 et 6 - NIVEAU 1.
 
 chaine = input("tapez votre texte")
 position = int(input("entrez la position désirée")) #int pour les nombre car pyton comprend pas
 caractere = input("tapez caractère de remplacement")
 
-def substituer(chaine,position,caractere):
-    liste = list(chaine)
+def substituer(chaine,position,caractere): faire une fonction à qui on attribue une chaine de caractère, la position du caractère que l'on souhaite changer et enfin le caractère de remplacement 
+    liste = list(chaine) # on ne peux pas modifier une chaine donc on crée une liste 
     liste[position] = caractere 
-    chaine = ''.join(liste)
+    chaine = ''.join(liste) # on remet la liste en chaine 
     return chaine 
 
 
