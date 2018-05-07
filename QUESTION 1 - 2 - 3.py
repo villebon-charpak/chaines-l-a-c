0ff7a8260ddb71c7d218d6a87191b4f5bf68e56c
Lucile <lucile.jacobe@u-psud.fr>
2018-05-07 16:47:35 +0200

    Revert "Merge branch 'master' of https://github.com/villebon-charpak/chaines-l-a-c"
    This reverts commit 787190ade58e5e7544908feed9c69402c1811214, reversing
    changes made to af06b3d744e809c138497445884e613106d6bad2.

--- QUESTION 1 - 2 - 3.py
+++ QUESTION 1 - 2 - 3.py
@@ -1,12 +1,36 @@
-#QUESTION 3 - NIVEAU 2
+#QUESTION 1 - NIVEAU 1
 
-def compter_mot(caractere, pouvoir):
-    compteur = 0
-    for word in range(len(pouvoir)):
-        if pouvoir[word] == caractere:
-            compteur += 1
-    print(compteur)
-    return compteur
+##exemplechaine = "Salut les amis ! On se retrouve caractere un nouveau tuto beauté proposé par Gemey Maybelline"
+##def compter_occurences(x):              #(x) = rpz notre argument
+##    A = 
+##    return A            #la fonction return nous permet de voir la valeur que nous donne la fonction
+## 
+##print(A)                #mettre print pour vraiment voir la valeur de la fonction
+##
+##
+##
+##chaine_y = "1,2,3,
+
+def carre(toto):
+    return toto**2
+
+###nombre_utilisateur = input("Entrez votre nombre\n")
+##nombre_utilisateur = 9
+##
+##x = 3
+##x = x**2
+##print (x)
+##
+##
+##n = carre(nombre_utilisateur)
+##print(n)
 
 
-compter_mot("lol", pouvoir)
+
+
+texte = "banane"
+print (texte[2])  #les crochets nous permettent d'afficher le 2e caractère. Attention, cela commence à 0 !
+longueur = len(texte)  #nous permet de compter le nombre de lettres dans ma chaine
+print (longueur)
+
+
