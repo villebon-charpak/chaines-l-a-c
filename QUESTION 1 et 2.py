583f9890e7aafd6a0a6e5a4a7fd6916af2656f6f
Alexia <alexia.ngono-ndjana@u-psud.fr>
2018-05-07 16:53:36 +0200

    rajout question 2

--- QUESTION 1 et 2.py
+++ QUESTION 1 et 2.py
@@ -4,6 +4,7 @@ Created on Mon May  7 15:06:42 2018
 
 @author: etudiant
 """
+#QUESTION 1
 
 chaine = input("Veuillez saisir du texte")
 
@@ -16,5 +17,21 @@ def compter_occurences(caractere, chaine):
     print(compteur)
     return compteur
 
-
 compter_occurences("u", chaine)
+
+#QUESTION 2
+
+chaine = input("Veuillez saisir du texte")
+
+def compter_occurences(espace, chaine):
+    compteur = 0
+    for i in range(len(chaine)):
+        if chaine[i] == espace:
+            compteur += 1
+            A = 1 + compteur
+    print(A)
+    return A
+
+
+compter_occurences(" ", chaine)
+
