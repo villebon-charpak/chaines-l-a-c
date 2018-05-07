af06b3d744e809c138497445884e613106d6bad2
Lucile <lucile.jacobe@u-psud.fr>
2018-05-07 16:47:09 +0200

    question 4
    hihi j'ai réussi

--- QUESTION 4 - 5 - 6.py
+++ QUESTION 4 - 5 - 6.py
@@ -1,4 +1,18 @@
 #QUESTION 4, 5 et 6 - NIVEAU 1
 
+chaine = input("tapez votre texte")
+position = int(input("entrez la position désirée")) #int pour les nombre car pyton comprend pas
+caractere = input("tapez caractère de remplacement")
+
+def substituer(chaine,position,caractere):
+    liste = list(chaine)
+    liste[position] = caractere 
+    chaine = ''.join(liste)
+    return chaine 
+
+
+chaine_modifiee = substituer(chaine,position,caractere)
+print(chaine_modifiee)
+    
 
 
