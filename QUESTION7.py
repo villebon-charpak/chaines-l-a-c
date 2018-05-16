309e742e07107da7c7d0ed8e760aff644e3c5244
Alexia <alexia.ngono-ndjana@u-psud.fr>
2018-05-16 18:37:01 +0200

    Compléments d'informations concernant les fonctions ord et chr

--- QUESTION7.py
+++ QUESTION7.py
@@ -13,8 +13,8 @@ caractere = input("Veuillez saisir une lettre:")
 decalage = int(input("Veuillez saisir un nombre:")) # int pour transformer le nombre en chaine de caractere (Python ne comprend que les chaines de caractere)
                                                     #str permet de prendre quelque chose comme une chaine de caractere aussi
 def decaler(caractere, decalage):
-    nombre = ord(caractere) + decalage
-    print(chr(nombre))
+    nombre = ord(caractere) + decalage # la fonction ord permet d'attribuer à un caractère un numéro grâce auq tables ASCII et Unicode
+    print(chr(nombre)) # l fonction chr permet de retrouver un caractère à partir de son numéro
     return chr(nombre)
 
 decaler(caractere, decalage)
