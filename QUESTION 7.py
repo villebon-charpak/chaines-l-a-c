8e7c93af57a2d753f121e1f204770e314556f499
Alexia <alexia.ngono-ndjana@u-psud.fr>
2018-05-16 16:27:42 +0200

    Précisions sur la question 7

--- QUESTION 7.py
+++ QUESTION 7.py
@@ -10,8 +10,8 @@ Created on Wed May 16 14:45:23 2018
 #Question 7
 
 caractere = input("Veuillez saisir une lettre:")
-decalage = int(input("Veuillez saisir un nombre:")) 
-
+decalage = int(input("Veuillez saisir un nombre:")) # int pour transformer le nombre en chaine de caractere (Python ne comprend que les chaines de caractere)
+                                                    #str permet de prendre quelque chose comme une chaine de caractere aussi
 def decaler(caractere, decalage):
     nombre = ord(caractere) + decalage
     print(chr(nombre))
