e7646dac7ebbd92da540e3f5245581d27070411b
Alexia <alexia.ngono-ndjana@u-psud.fr>
2018-05-22 16:39:59 +0200

    chiffrement et déchiffrement de César

--- blabla.py
+++ blabla.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed May 16 16:34:35 2018
+Created on Tue May 22 16:03:35 2018
 
 @author: etudiant
 """
-from QUESTION7 import decaler       # nous importons le fichier dans lequel il y a la fonction qui nous intéresse, à savoir la fonction decaler
-    
+
+def decaler(caractere, decalage):        # pour éviter d'avoir à redéclarer la fonction, on aurait pu donner le nom de l'exercice dans lequel c'est défini (pas possible ici car le nom du fichier a un espace)
+    nombre = ord(caractere) + decalage
+    return chr(nombre)
 
 def chiffrement_cesar(chaine_de_caractere, cle):
     chiff = ""      #on a nommé cette variable pour pouvoir l'utiliser ensuite et elle vide parce qu'elle va se remplir au fur et à mesure que la chaine de caractères sera chiffrée
     for char in chaine_de_caractere:  #char permet de s'intéresser à chaque caractère
-        chiff = chiff+QUESTION7.decaler(char,cle)  # comme on utilise la fonction decaler, il faut la précéder du nom du chier dans laquelle elle est suivie d'un point 
+        chiff = chiff+decaler(char,cle)
     print(chiff)
     return chiff
 
 chaine_de_caractere = input("Veuillez saisir votre texte:")
 cle = int(input("Veuillez saisir un nombre entier:"))
 chiffrement_cesar(chaine_de_caractere,cle)
-
-
