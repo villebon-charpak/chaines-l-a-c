2f4596a4b1850b288eb67a8f01b1d2bc8917ac60
Alexia <alexia.ngono-ndjana@u-psud.fr>
2018-05-22 22:20:07 +0200

    Import seulement de la fonction pour éviter de refaire tout l'exercice mais avoir seulement la fonction
    Pour éviter de refaire tout l'exercice mais avoir seulement la fonction souhaitée

--- QUESTION 8.py
+++ QUESTION 8.py
@@ -4,7 +4,7 @@ Created on Wed May 16 16:34:35 2018
 
 @author: etudiant
 """
-from QUESTION7 import decaler       # nous importons le fichier dans lequel il y a la fonction qui nous intéresse, à savoir la fonction decaler
+import QUESTION7       # nous importons le fichier dans lequel il y a la fonction qui nous intéresse, à savoir la fonction decaler
     
 
 def chiffrement_cesar(chaine_de_caractere, cle):
