3f50e08d74317c2b04589a68547a6e5c590029be
CyrineSA <38528481+CyrineSA@users.noreply.github.com>
2018-05-22 14:41:12 +0200

    Correction de la question 3
    On a pu compter le nombre de fois qu'apparait le mot lol
    Cela permet de  définir dans notre variable chaine le mot que l'on veut détecter

--- QUESTION 1 et 2 et 3.py
+++ QUESTION 1 et 2 et 3.py
@@ -35,19 +35,17 @@ def compter_mots(chaine):
 
 compter_occurences(" ", chaine)
 
-#QUESTION 3 - NIVEAU 2
+##QUESTION 3 - NIVEAU 2
 
-##chaine = input("Veuillez saisir du texte\n")
-##
-##
-##def compter_mot(mot, chaine):
-##    compteur = 0
-##    for i in range(len(chaine)):
-##        if chaine[i] == mot:
-##            compteur += 1
-##    print(compteur)
-##    return compteur
-##
-##compter_occurences("lol", chaine)
+chaine = input("Veuillez saisir du texte\n")
 
-#n'a pas l'air de marcher pour l'instant, j'essaierai de le modidfier encore
+
+def compter_mot(mot, chaine):
+    compteur = 0
+    for i in range(len(chaine)):
+        if chaine[i:i+3] == mot:                #Pour compter le nombre de fois qu'apparait le lol, on applique la lecture de 3 lettres en 3 lettres grâce à [i:i+3], et ne pas poublier qu'en Python , on applique une borne supérieure stricte et surtout qu'on ajoute un +1 à notre borne
+            compteur += 1
+    print(compteur)
+    return compteur
+
+print(compter_mot("lol", chaine))               #Ne pas oublier à définir notre x et la chaine (le y) 
